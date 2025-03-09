2181739beff070ad79ccdffa65521d98466467ae
temurbek2310 <ghostwire2010@gmail.com>
2025-03-09 11:48:29 +0500

    deployment to vercel

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.swgha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const main = async (req, res) => {
+  res.send("Hello World!");
   // CONNECTION TO DB
   await mongoose
     .connect(URI)
